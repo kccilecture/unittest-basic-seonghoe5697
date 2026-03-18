9b271b8f33081db35b66742537ba2872746e0684
csh <choesh5697@gmail.com>
2026-03-18 16:59:42 +0900

    unitest

---
+++ unittest/my_func.py
@@ -0,0 +1,31 @@
+def even_odd(num):
+    if(num % 2 == 0):
+        return True
+    else:
+        return False
+
+def avg(li_avg):
+    sum = 0 
+    for i in li_avg:
+        sum += i 
+    return sum / len(li_avg)
+    
+
+def max_val(n):
+    max = n[0]
+    for i in range(1,len(n)):
+        if n[i] > max
+            max = n[i] 
+    return max   
+                     
+def min_val(n):
+    min = n[0]
+    for i in range(1,len(n)):
+        if n[i] < min:
+            min =n[i]
+    return min
+
+
+
+
+
--- unittest/test_funcs.py
+++ unittest/test_funcs.py
@@ -1,6 +1,17 @@
 # TODO: 사용자 모듈 import
-
+from my_func import even_odd,avg,max_val,min_val
 
 # TODO: 아래의 코드를 삭제하고 unittest를 작성하세요.
-def test_always():
-    assert False, "Remove me!"
+def test_even_odd():
+    assert True == even_odd(2)
+    assert False == even_odd(3)
+
+li = [2,4,6,8]
+def test_average():
+    assert 5 == avg(li)
+
+def test_max_val():
+    assert 8 == max_val(li)
+
+def test_min_val():
+    assert 2 == min_val(li)
